84f9025b05d8bd89c4696eb961bb3f201f8e062b
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-09-08 22:13:00 +0600

    completed

--- Day-6/Maze.py
+++ Day-6/Maze.py
@@ -1,7 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    turn_left()
 
-move()
-turn_left()
-
-
-while not at_goel():
-    if 
+while not at_goal():
+    if wall_in_front():
+        turn_right() 
+    elif right_is_clear():
+        #turn_left()
+        move()
+        turn_left()
+        
+        
+    else:
+        move()
+        
+        
+        
