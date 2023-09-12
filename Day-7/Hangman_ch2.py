a64ad109caf0254d3f1037cac307504a59047de4
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-09-12 11:46:57 +0600

    completed

--- Day-7/Hangman_ch2.py
+++ Day-7/Hangman_ch2.py
@@ -1,41 +1,30 @@
-#Step 2
-
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
-#Testing code
+# Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-display = []
-for letter in chosen_word:
-  display.append("_")
+# TODO-1: Create an empty list called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = ["_" for _ in chosen_word]
 
-print(display)
 end_of_game = False
 while not end_of_game:
-  guess = input("Guess a letter: ").lower()
+    guess = input("Guess a letter: ").lower()
 
-  #TODO-2: - Loop through each position in the chosen_word;
-  #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-  #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+    # TODO-2: Loop through each position in the chosen_word;
+    # If the letter at that position matches 'guess', then reveal that letter in the display at that position.
+    # e.g., If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-  position = 0
-  for letter in chosen_word:
-        if letter == guess:
-            index = chosen_word.index(letter,position)
-            display[index] = letter
-            position = position + 1
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            display[position] = guess
 
+    print(display)
 
-  print(display)
-
-if "_" not in display:
-  end_of_game = True
-  print("You win!!!")
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+    if "_" not in display:
+        end_of_game = True
+        print("You win!!!")
