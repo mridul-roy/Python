04b814f2adada9f52379ee8a76fd886a41d9c70b
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-09-23 16:34:57 +0600

    guess_a_number game completed

--- Day-12/number_gassing_game.py
+++ Day-12/number_gassing_game.py
@@ -1,21 +1,45 @@
 import random
+from art import logo
 
-computer_select = random.randint(2,100)
-print(f"Computer Gussed answer is :{computer_select}")
-flag = True
+EASY_LEVEL_TURN = 10
+HARD_LEVEL_TURN = 5
 
-number = int(input("Gassed a number: "))
-easy_attempt_count = 0
-for number in range (5):
-    def level_easy(number):
-        if number == computer_select:
-            return f"You win.Picked number matched with computer choose"
-        elif number > computer_select:
-            return "Number is too high"
-        elif number < computer_select:
-            return "Number is too less"
-           
 
-level_easy(number)
-result = level_easy(number)
-print(result)
+def game_level():
+    set_level = input("Choose game level 'easy' or 'hard' :")
+    if set_level == "easy":
+        return EASY_LEVEL_TURN
+    elif set_level == "hard":
+        return HARD_LEVEL_TURN
+
+def num_check(computer_select,guess_number,turns):
+    if computer_select > guess_number:
+        print("It's too low")
+        return turns - 1
+    elif computer_select < guess_number:
+        print("It's too high")
+        return turns - 1
+    else:
+        print("It's matched.You win.")
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    computer_select = random.randint(2,100)
+    #print(f"Computer Guessd answer is :{computer_select}")
+
+    turns = game_level()
+    
+    guess_number = 0
+
+    while guess_number != computer_select :
+        print(f"You have {turns} attempt to go with.")
+        guess_number = int(input("Gassed a number: "))
+        turns = num_check(computer_select,guess_number,turns)
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess_number != computer_select:
+            print("Guess again")
+game()
