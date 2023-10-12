bb843a7b2a451e128f9d5fa842d536fc6ec8ef65
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-10-12 19:00:01 +0600

    Exception Handling

---
+++ Day-30/IndexErrorHandling.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "orange"]
+# 🚨 Do not change the code above
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+# 🚨 Do not change the code below
+make_pie(2)
