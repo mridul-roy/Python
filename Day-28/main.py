3f65ab00d57c0ad5b2050fd89f83fcff43761558
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-10-13 22:34:27 +0600

    Pomodoro Completed

--- Day-28/main.py
+++ Day-28/main.py
@@ -79,6 +79,7 @@ def count_down(count):
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
+
 canvas_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
 
