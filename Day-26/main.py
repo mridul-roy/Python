08724dffd42ffe4979ec6d7387ac398b1d56026e
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-10-22 11:54:20 +0600

    completed

--- Day-26/main.py
+++ Day-26/main.py
@@ -4,8 +4,16 @@ nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 data = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 
-word = input("Write your name : ").upper()
 
-output = [data[letter] for letter in word]
+def take_input():
+    word = input("Write your name :").upper()
+    try:
+        output = [data[letter] for letter in word]
+    except KeyError:
+        print("Sorry, Entered only text")
+        take_input()
+    else:
+        print(output)
 
-print(output)
+
+take_input()
