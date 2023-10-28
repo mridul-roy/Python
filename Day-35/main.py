fc6f5f7410d2876933fcf91e1ff420ac1c5a0448
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-10-29 02:12:18 +0600

    Habit Tracker App Completed with pixela

--- Day-35/main.py
+++ Day-35/main.py
@@ -3,15 +3,15 @@ import requests
 API_key = "58833639263de97074a9f2214c06e0ad"
 
 
-END_point = "https://api.openweathermap.org/data/2.5/weather"
-#END_point = "https://api.openweathermap.org/data/2.5/onecall"
+#END_point = "https://api.openweathermap.org/data/2.5/weather"
+END_point = "https://api.openweathermap.org/data/2.5/onecall"
 
 
 weather_params = {
     "lat": 24.848078,
     "lon": 89.372963,
     "appid": API_key,
-    "exclude": "current,minutely,hourly"
+
 
 }
 
