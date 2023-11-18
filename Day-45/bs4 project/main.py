e55ff47c71a2982c49cd08fb25ef9d554b546dd8
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-11-18 23:57:31 +0600

    init

--- Day-45/bs4 project/main.py
+++ Day-45/bs4 project/main.py
@@ -1,9 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
 
+response = requests.get("https://news.ycombinator.com/")
+yc_webpage = response.text
 
-with open(file="website.html", encoding="utf8")as file:
-    contents = file.read()
+soup = BeautifulSoup(yc_webpage, "html.parser")
 
-soup = BeautifulSoup(contents, "html.parser")
+article_tag = soup.find(name='a', rel='noreferrer')
+print(article_tag.getText())
 
-print(soup.find_all())
+# print(soup.find_all('a'))
+
+# with open(file="website.html", encoding="utf8")as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+#
+# print(soup.find_all())
