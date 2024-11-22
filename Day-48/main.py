0bd111db1a47a4e7c9f2b77b97814cf84422b0e9
mridul <mriduroy010@gmail.com>
2024-11-22 18:50:07 +0600

    Modifited

--- Day-48/main.py
+++ Day-48/main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 chrome_option = webdriver.ChromeOptions()
-chrome_option.add_experimental_option("detach", True)
+#chrome_option.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_option)
 driver.get(
